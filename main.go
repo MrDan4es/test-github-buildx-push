22275bcfe458f348c8c55a9c83e8fa600fde9f60
Morozov Danil <d.morozov@infinet.ru>
2024-02-28 11:25:05 +0500

    added http server & docker-compose

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 )
 
+const addr = ":3333"
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	_, err := io.WriteString(w, "HELLO")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
 func main() {
-	fmt.Println("Hello World FROM DOCKER 1000000 FIXES AWAY")
+	http.HandleFunc("/", getHello)
+
+	fmt.Printf("server starting on port %s\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		panic(err)
+	}
 }
