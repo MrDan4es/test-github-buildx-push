4a53587e4324efb8f0c4db7576dc0db984287ce4
Murawev <saviorlex@gmail.com>
2024-03-05 23:19:56 +0500

    SCORE OUR WORK

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 const addr = ":3333"
 
 func getHello(w http.ResponseWriter, r *http.Request) {
-	_, err := io.WriteString(w, "IT's REEBUILDED AND PUSHED AND RESTARDED BY PIPELINE!!!!!!!!!!!!!!!!!!!!!")
+	_, err := io.WriteString(w, "5")
 	if err != nil {
 		fmt.Println(err)
 	}
